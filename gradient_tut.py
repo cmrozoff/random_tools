55f11b328116def7b159062b05c191301a84fe89
Christopher Rozoff <rozoff@cheyenne6.cheyenne.ucar.edu>
2020-12-09 14:09:50 -0700

    Add files

---
+++ gradient_tut.py
@@ -0,0 +1,25 @@
+import scipy.ndimage as nd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+import sys
+
+x = np.arange(0, 10)
+y = np.arange(0, 10)
+X, Y = np.meshgrid(x, y)
+Z = np.sin(X-1) + 0.0*np.cos(Y-1)
+#
+Z2 = nd.laplace(Z, output=None, mode='wrap')
+Z2[0,]=Z2[1,]
+Z2[-1,]=Z2[-2,]
+Z2[:,0]=Z2[:,1]
+Z2[:,-1]=Z2[:,-2]
+#
+grady, gradx = np.gradient(Z, 1, 1)
+#
+sys.exit()
+fig = plt.figure()
+cs = plt.contourf(X, Y, gradx, cmap=cm.PuBu_r)
+cbar = fig.colorbar(cs)
+plt.show()
+#
